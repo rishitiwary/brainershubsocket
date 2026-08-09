8b0276522e20725f4f97299e814cb1f22f956365
rishitiwary <rishitiwary694076@gmail.com>
2026-08-09 13:19:40 +0530

    fixed connection issue

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ module.exports = {
     host: process.env.DB_HOST || '127.0.0.1',
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD !== undefined ? process.env.DB_PASSWORD : '',
     database: process.env.DB_DATABASE || 'brainerhub',
     waitForConnections: true,
     connectionLimit: 10,
